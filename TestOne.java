45a16b3a0e4cac6bb612fb87642e4cb904176a4b
Drewsh01 <drewsh01@gmail.com>
2018-11-29 08:30:37 -0500

    Test- Multiple print statements
    Prints test
    No bugs

---
+++ TestOne.java
@@ -0,0 +1,15 @@
+public class TestOne{
+	public static void main(String [] args){
+
+	System.out.println("It's Day Two");
+
+	System.out.println("Yay!");
+
+	System.out.println("Testing: Partner A to B");
+
+	System.out.println("Drew Shah");
+
+
+	}
+}
+
